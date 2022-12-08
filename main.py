589a87c8acf7160852fcd4cba2ebc81cabb26759
jimvanoosten <vanoostenjim@gmail.com>
2022-12-08 10:47:30 +0100

    test commit

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os 
+import numpy as np 
+import matplotlib.pyplot as plt
 os.chdir('C:/Users/jimvanoosten/.spyder-py3/Operations')
 cwd = os.getcwd()
 print(cwd)
 
 print("hello world")
+
+x = np.random.randint(8, size=(4, 4))
+print(x)
+
+y = np.random.randint(6, size=(4, 4))
+print(y)
+
+plt.plot(x, y)
